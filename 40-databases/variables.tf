995f7c1cf56b1bf68caf25bbd18a4e0809038f85
shirishakodari <kodarishirisha419@gmail.com>
2026-06-07 13:44:14 +0530

    update

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -1,3 +1,20 @@
+# variable "project" {
+#     default = "roboshop"
+# }
+
+# variable "environment" {
+#     default = "dev"
+# }
+
+
+# variable "zone_id" {
+#     default = "Z0673912SPW6GV3I8NW2"
+# }
+
+# variable "zone_name" {
+#     default = "daws84s.cfd"
+# }
+
 variable "project" {
     default = "roboshop"
 }
@@ -6,11 +23,11 @@ variable "environment" {
     default = "dev"
 }
 
-
 variable "zone_id" {
-    default = "Z0673912SPW6GV3I8NW2"
+    default = "Z0342386ADU8CPI7S8K"
 }
 
 variable "zone_name" {
-    default = "daws84s.cfd"
+    default = "kodari.online"
 }
+  
