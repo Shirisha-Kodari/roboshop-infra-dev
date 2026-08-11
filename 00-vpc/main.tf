4c399238bbe59c73aadea69d6a0556aceb4a114a
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-08-11 10:55:41 +0530

    Update main.tf

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
       
     project = "roboshop"
     environment = var.environment
-    public_subnet_cidrs = var.public_subnet_cidrs
+    public_subnet_cidrs = var.public_subnet_cidrs 
     private_subnet_cidrs = var.private_subnet_cidrs 
     database_subnet_cidrs = var.database_subnet_cidrs
 
