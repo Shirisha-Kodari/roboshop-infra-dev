5eb229ae596879156df4cfd423e8230dc0325305
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-08-11 10:57:24 +0530

    Update main.tf

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
     environment = var.environment
     public_subnet_cidrs = var.public_subnet_cidrs 
     private_subnet_cidrs = var.private_subnet_cidrs 
-    database_subnet_cidrs = var.database_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs 
 
     is_peering_required = false
 
